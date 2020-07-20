73369075539cb219ae6f0b547beb1e212c050078
Loïs Bégué <3593197+khatastroffik@users.noreply.github.com>
2020-07-20 14:33:26 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'k11k-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tetris';
+}
