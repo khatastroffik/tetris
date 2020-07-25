7ae6bc2dc36b7aece0d2c2a5ce635c314732a2e1
Loïs Bégué <3593197+khatastroffik@users.noreply.github.com>
2020-07-25 10:22:19 +0200

    feature: implement responsive UI using CSS grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
-@Component({
+type Grid = number[][];
+
+@Component( {
   selector: 'k11k-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
-})
+} )
 export class AppComponent {
-  title = 'tetris';
+  title = 'Tetris';
+  grid: Grid;
+  constructor() {
+    this.grid = Array( 20 ).fill( Array( 10 ).fill( 3, 0 ), 0 );
+    this.grid = Array( 20 ).fill( 0 ).map( _ => Array( 10 ).fill( 0 ) );
+
+
+    this.grid[2][2] = 1;
+    this.grid[3][2] = 1;
+    this.grid[3][3] = 1;
+    this.grid[3][4] = 1;
+    console.table( this.grid );
+  }
 }
